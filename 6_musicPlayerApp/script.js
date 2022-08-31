d906d947b2f1d8583906134e8db5dd1cd3da1d5e
charlybgood <bonavita.carlos@gmail.com>
2022-08-31 15:05:31 -0300

    clean code and add comments on js

--- 6_musicPlayerApp/script.js
+++ 6_musicPlayerApp/script.js
@@ -1,3 +1,4 @@
+// create songs list array
 const songs = [
   "charlybgood-music-1.mp3",
   "charlybgood-music-2.mp3",
@@ -7,17 +8,18 @@ const songs = [
   "charlybgood-music-6.mp3",
 ];
 
+// select player buttons and elements
+let playerImg = document.querySelector(".radio");
 let playBtn = document.getElementById("play");
 let pauseBtn = document.getElementById("pause");
 let stopBtn = document.getElementById("stop");
-let playerImg = document.querySelector(".radio");
-
 const progressBar = document.getElementById("progress");
 
+// select audio input
 const player = document.getElementById("player");
-
 const source = document.getElementById("source");
 
+// select element to show current file name and create list of available songs
 const currentSong = document.querySelector("#currentSong");
 
 function createSongList() {
@@ -38,36 +40,42 @@ for (const link of links) {
   link.addEventListener("click", setSong);  
 }
 
+// when called the selected file will be played
 function setSong(e) {
+  let titleS = e.target.innerText;
   playerImg.classList.remove("pulse");
-  source.src = "songs/" + e.target.innerText;  
+  source.src = "songs/" + titleS;  
   e.target.style.background = "#3a063e"
-//   currentSong.innerText = `Now playing: ${e.target.innerText}`;
+  currentSong.innerText = titleS;
   player.load();
   player.play();
   playerImg.classList.add("pulse");  
 }
 
+// add click event on play button
 playBtn.addEventListener("click", () => {
   player.readyState
     ? player.play()
     : console.log("your motherfkn function works!");
 });
 
+// add click event on pause button
 pauseBtn.addEventListener("click", () => player.pause());
 
+// add click event on stop button
 stopBtn.addEventListener("click", () => {
   player.load();
   progressBar.value = "0";
 });
 
+// add volume slider input functionality
 const slider = document.getElementById("volumeSlider");
 slider.oninput = function (e) {
-  const volume = e.target.value;
-  console.log(e.target.value);
+  const volume = e.target.value;  
   player.volume = volume;
 };
 
+// update state on progress bar when file is playing
 function updateProgress() {
   if (player.currentTime > 0) {
     progressBar.value = (player.currentTime / player.duration) * 100;
