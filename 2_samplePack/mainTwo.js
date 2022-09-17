8e3877eddf94ac6f891946a6607353810fb60389
charlybgood <bonavita.carlos@gmail.com>
2022-09-16 21:18:55 -0300

    try different values for oscillator sound values

--- 2_samplePack/mainTwo.js
+++ 2_samplePack/mainTwo.js
@@ -6,12 +6,21 @@ const NOTES = {
   "E-4": 329.628,
   "F-4": 349.228,
   "G-4": 391.995,
-  "A-4": 440.000,
+  "A-4": 440.0,
   "B-4": 493.883,
   "C-5": 523.251,
-}
+};
+
+const WAVEFORMS = ["sine", "square", "sawtooth", "triangle"];
 
 let osc;
+
+const waveSlider = document.getElementById("waveSlider");
+// waveSlider.addEventListener("input", (e) => {
+//   const val = e.target.value;
+//   oscillator.frequency.value = val * 400;
+// })
+
 
 const pads = drumz.querySelectorAll(".key");
 for (let i = 0; i < pads.length; i++) {
@@ -23,11 +32,11 @@ for (const pad of pads) {
     const actx = new (AudioContext || webkitAudioContext)();
     if (!actx) throw "Not supported!";
     osc = actx.createOscillator();
-    osc.type = "sine";
+    osc.type = "triangle";
     osc.frequency.value = NOTES[pad.dataset.note]; // Hz = middle A
     osc.connect(actx.destination);
-    osc.start();    
-    pad.classList.add("playing");    
+    osc.start();
+    pad.classList.add("playing");
   });
 
   pad.addEventListener("pointerup", () => {
