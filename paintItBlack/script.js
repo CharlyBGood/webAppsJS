0b1eb62d3295a32d63bb5daf7ff02333b1f6b291
charlybgood <bonavita.carlos@gmail.com>
2023-03-03 19:53:07 -0300

    add new JS cleaner code

--- paintItBlack/script.js
+++ paintItBlack/script.js
@@ -1,169 +1,209 @@
-// import * as randomColor from "./randomColor.js";
+let infoBtn = document.getElementById("click-me-info");
+let alienFriend = document.getElementById("alien");
 
-// crear canvas 2d
-let canvas = document.getElementById("area");
-let ctx = canvas.getContext("2d");
+// animate info button for message
+const animationBeat = {
+  keyframe: [
+    { transform: "scale(1)" },
+    { transform: "scale(1.2)" },
+    { transform: "scale(1)" },
+  ],
+  options: {
+    duration: 1000,
+    iterations: Infinity,
+  },
+};
 
-// crear variables de posición de pincel y estado de dibujo
-let stage;
-let x;
-let y;
-let colorX = "#f3ebdc";
+infoBtn.animate(animationBeat.keyframe, animationBeat.options);
+alienFriend.animate(animationBeat.keyframe, animationBeat.options);
+infoBtn.addEventListener("click", magikInfo);
+
+function magikInfo() {
+  alert("Prueba dibujando con varios dedos a la vez!!");
+}
+
+// start to work on canvas
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d", { willReadFrequently: true });
+
+// sixe of the brush
 let size;
-let magicButton = false;
-let bgrBtn1 = document.getElementById("bgr_hide");
-let bshBtn = document.getElementById("bsh_hide");
-let lastAction;
 
-// definir tamaño de canvas segun window
+// create variable for events on brush size / range input
+let increaseBrush = document.getElementById("range");
+increaseBrush.addEventListener("input", brushSize);
+
+function brushSize() {
+  size = increaseBrush.value;
+  console.log(size);
+}
+
+// define canvas width and height according window object
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-
-// añadir eventos de mouse / touch
-canvas.addEventListener("pointerdown", pointerDown, false);
-canvas.addEventListener("pointermove", pointerMove, false);
-canvas.addEventListener("pointerup", pointerUp, false);
-
-// Generar color random 
-function rand(min, max) {
-  let randomNumbs = min + Math.random() * (max - min);
-  return randomNumbs;
-}
-
-let colorZ = function randColorHsl() {
-  let h = rand(0, 360);
-  let s = rand(25, 100);
-  let l = rand(15, 75);
-  return 'hsl(' + h + ',' + s + '%,' + l + '%)';
-}
-
-// redimensionar canvas según viewport
 window.addEventListener("resize", function () {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
 });
 
-// cambiar color de fondo al canvas
-let bgrPckr = document.getElementById("bgr_color");
-bgrPckr.addEventListener("input", bgrChange);
+// color and background-color buttons behaviour
+let bgColorBtn = document.getElementById("bgr_color_btn");
+let bgColorInput = document.getElementById("bgr_color_input");
 
-function bgrChange() {
-  canvas.style.backgroundColor = bgrPckr.value;
-  bgrBtn1.style.backgroundColor = bgrPckr.value;
+bgColorBtn.addEventListener("click", () => {
+  bgColorInput.click();
+});
+
+bgColorInput.addEventListener("input", () => {
+  bgColorBtn.style.backgroundColor = bgColorInput.value;
+  canvas.style.backgroundColor = bgColorInput.value;
+});
+
+// brush color handler
+let paintColor = "#fb2359"; //default color on start
+let brushColBtn = document.getElementById("brush_color_btn");
+let brushColor = document.getElementById("brush_color_input");
+
+// brush handler for magikPaintingcolor
+let magikBtn = document.getElementById("magic_button");
+let magikPainting = false;
+
+// eraser button
+let eraserBtn = document.getElementById("eraser_btn");
+
+// generate random numbers to create random hsl color
+function randomValue(min, max) {
+  let randomNumbs = min + Math.floor(Math.random() * (max - min));
+  return randomNumbs;
 }
+// generate random color for magik brush!!
+const magikColor = () => {
+  let h = randomValue(0, 360);
+  let s = randomValue(25, 100);
+  let l = randomValue(15, 75);
+  return `hsl(${h},${s}%,${l}%)`;
+};
 
-// cambiar color a pincel
-let brushPckr = document.getElementById("brush_color");
-brushPckr.addEventListener("input", brushColor);
-
-function brushColor() {
-  colorX = brushPckr.value;
+// select "magik random color" for the brush!
+magikBtn.addEventListener("click", () => {
+  magikPainting = !magikPainting;
   ctx.globalCompositeOperation = "source-over";
-  bshBtn.style.backgroundColor = brushPckr.value;
-  magicButton = false;
-}
+});
 
-// seleccionar pincel mágico
-let brushMagic = document.getElementById("magic_button");
-brushMagic.addEventListener("click", brushMc);
+// select different color for brush with color picker
+brushColBtn.addEventListener("click", () => {
+  brushColor.click();
+});
 
-function brushMc() {
-  
-  magicButton = !magicButton;
-}
+brushColor.addEventListener("input", () => {
+  brushColBtn.style.backgroundColor = brushColor.value;
+  paintColor = brushColor.value;
+  magikPainting = false;
+  ctx.globalCompositeOperation = "source-over";
+});
 
-// seleccionar goma de borrar
-let brushErase = document.getElementById("brush_erase");
-brushErase.addEventListener("click", eraseDraw);
+// use de eraser to erase selected parts of the drawing !!
+eraserBtn.addEventListener("click", eraseSelection);
 
-function eraseDraw() {
+function eraseSelection() {
   ctx.globalCompositeOperation = "destination-out";
-  magicButton = false;
+  magikPainting = false;
 }
 
-// cambiar tamaño a pincel
-let sizeB = document.getElementById("range");
-sizeB.addEventListener("input", brushSize);
+// clear the canvas to start over again
+let clearBtn = document.getElementById("clear_canvas");
+clearBtn.addEventListener("click", clearCanvas);
 
-function brushSize() {
-  size = sizeB.value;
-}
-
-// recargar canvas
-let reload = document.getElementById("reload");
-reload.addEventListener("click", reloadCanvas);
-
-function reloadCanvas() {
+function clearCanvas() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  bshBtn.style.backgroundColor = "#12e671d0";
-  bgrBtn1.style.backgroundColor = "#e69b12d0";
-  brushPckr.value = "#000000";
-  bgrPckr.value = "#000000";
+  restore_arr = [];
+  index = -1;
 }
 
-// deshacer trazo
+// undo button to get a step back on the drow !!
+let undoBtn = document.getElementById("undo_trace");
 
-let undoBtn = document.getElementById("undo");
-undoBtn.addEventListener("click", undoTrace);
+let restore_arr = [];
+let index = -1;
+let deletedTrace = [];
 
-function undoTrace(e) {
-  // e.preventDefault()
-  pointerMove(lastAction)
-  
-  console.log(lastAction)
-  
-}
-
-// funciones para dibujar según se mueve el puntero
-function pointerDown(ev) {
-  stage = 1;
-  x = ev.layerX;
-  y = ev.layerY;
-  console.log(ev.x)
-  ev.preventDefault();
-}
-
-function pointerMove(ev) {
-  if (stage == 1) {
-    if (magicButton) {
-      colorX = colorZ();
-      ctx.globalCompositeOperation = "source-over";
-    }
-    drawLine(colorX, x, y, ev.layerX, ev.layerY, ctx);
-    
+undoBtn.addEventListener("click", () => {
+  if (index <= 0) {
+    clearCanvas();
+    index = -1;
+  } else {
+    index--;
+    restore_arr.pop();
+    ctx.putImageData(restore_arr[index], 0, 0);
   }
-  x = ev.layerX;
-  y = ev.layerY;
+});
+
+// redo button to recover the undo trace
+// let redoBtn = document.getElementById("redo_trace");
+
+// redoBtn.addEventListener("click", () => {
+//   console.log(deletedTrace[index])
+// });
+
+// save draw when done
+let saveBtn = document.getElementById("save_draw");
+saveBtn.addEventListener("click", saveDraw);
+
+function saveDraw(a) {
+  let img = canvas.toDataURL("img/png", 1.0);
+  downloadImage(img, "my_draw.png");
 }
 
-function pointerUp() {
+function downloadImage(data, filename = "untitled.png") {
+  let a = document.createElement("a");
+  a.href = data;
+  a.download = filename;
+  document.body.appendChild(a);
+  a.click();
+}
+
+// add mouse and touch events on canvas to draw
+canvas.addEventListener("pointerdown", pointerDown, false);
+canvas.addEventListener("pointermove", pointerMove, false);
+canvas.addEventListener("pointerup", pointerUp, false);
+
+// boolean value to update on pointer event true/false
+let stage;
+let x;
+let y;
+
+function pointerDown(e) {
+  stage = 1;
+  x = e.offsetX;
+  y = e.offsetY;
+  e.preventDefault();
+}
+
+function pointerMove(e) {
+  if (stage == 1) {
+    if (magikPainting) {
+      paintColor = magikColor();
+    }
+    drawLine(paintColor, x, y, e.offsetX, e.offsetY, ctx);
+  }
+  x = e.offsetX;
+  y = e.offsetY;
+}
+
+function pointerUp(e) {
   stage = 0;
+  restore_arr.push(ctx.getImageData(0, 0, canvas.width, canvas.height));
+  index += 1;
 }
 
-function drawLine(color, xini, yini, xfin, yfin, ctx) {
+// function to draw on canvas
+function drawLine(color, xstart, ystart, xend, yend, ctx) {
   ctx.beginPath();
   ctx.strokeStyle = color;
   ctx.lineWidth = size;
-  ctx.moveTo(xini, yini);
-  ctx.lineTo(xfin, yfin);
+  ctx.moveTo(xstart, ystart);
+  ctx.lineTo(xend, yend);
+  ctx.lineCap = "round";
   ctx.stroke();
   ctx.closePath();
 }
-
-// NAVBAR
-
-// window.onscroll = function() {smoothFunc()};
-
-// let navbar = document.getElementById('container');
-
-// let sticky = navbar.offsetTop;
-
-// function smoothFunc() {
-//     if (window.pageYOffset) {
-//         navbar.classList.add('sticky');
-//     } else {
-//         navbar.classList.remove('sticky');
-//     }
-// }
-
-// -------------------
